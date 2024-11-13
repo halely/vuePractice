ac8632f49de3c9ff1b4b28a6dbb260df8e81a3e5
刘勇 <nfc5617@dingtalk.com>
2024-11-13 15:13:23 +0800

    :tada: 第一次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper ">
+      <nav>
+        <RouterLink to="/">家庭</RouterLink>
+        <RouterLink to="/about">关于</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+// https://vite.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+    // 服务设置
+    server: {
+      host: true, // host设置为true才可以使用network的形式，以ip访问项目
+      port: 8181, // 端口号
+    },
+  }
+})
